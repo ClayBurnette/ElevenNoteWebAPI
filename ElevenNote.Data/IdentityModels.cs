8d4c138a6f91707243ff22885c3f7bdb516e5d77
Clay Burnette <clayburnette13@gmail.com>
2022-04-03 19:51:11 -0400

    Set Up Webconfig

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using ElevenNote.Data;
+
 
 namespace ElevenNote.Data
 {
