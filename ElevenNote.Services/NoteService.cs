6f11d24ee6d1f32fea95f10a5119498cce4a26bd
Clay Burnette <clayburnette13@gmail.com>
2022-04-03 21:15:25 -0400

    Note Service Updated

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -29,7 +29,7 @@ namespace ElevenNote.Services
                 ctx.Notes.Add(entity);
                 return ctx.SaveChanges() == 1;
             }
-        } 
+        }
         public IEnumerable<NoteListItem> GetNotes()
         {
             using (var ctx = new ApplicationDbContext())
@@ -46,6 +46,24 @@ namespace ElevenNote.Services
                     }
             );
                 return query.ToArray();
+            }
+        }
+        public NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx
+                    .Notes
+                    .Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return
+                new NoteDetail
+                {
+                    NoteId = entity.NoteId,
+                    Title = entity.Title,
+                    Content = entity.Content,
+                    CreatedUtc = entity.CreatedUtc,
+                    ModifiedUtc = entity.ModifiedUtc
+                };
             }
         }
     }
